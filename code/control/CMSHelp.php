a800bc2614c8a35065fe70d697d932f50103a6eb
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2012-12-26 10:19:43 +0000

    UPGRADE 3.0
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/templateoverview/trunk@4361 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/control/CMSHelp.php
+++ code/control/CMSHelp.php
@@ -30,7 +30,7 @@ class CMSHelp extends ContentController {
 			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
 		}
 		parent::init();
-		Requirements::themedCSS("typography");
+		Requirements::themedCSS("typography", "typography");
 		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
 	}
 
