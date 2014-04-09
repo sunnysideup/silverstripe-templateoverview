e73262b4d46617ac1b3f3a3c32bdd31bf1a6c958
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-09 05:13:09 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/templateoverview/trunk@5593 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -8,24 +8,4 @@
 
 Config::inst()->update("ErrorPage", 'icon', "templateoverview/images/treeicons/ErrorPage");
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START templateoverview MODULE ----------------===================
-//MUST SET
-//TemplateOverviewBug::set_error_email("a@b.com");
-//if(Director::isDev()) {
-  //Object::add_extension('Page_Controller', 'TemplateOverviewPageExtension');
-	//MAY SET
-  //Object::add_extension('SiteTree', 'TemplateOverviewPageDecorator');
-  //Director::addRules(100, array('error/report' => 'ErrorNotifierController'));
-//}
-//TemplateOverviewPage::set_auto_include(true);
-//LeftAndMain::require_css("templateoverview/css/TemplateOverviewCMSHelp.css");
-//any file in the folder below which starts with the Exact ClassName - e.g. HomePage or HomePage1 or HomePage_MoreDetail
-//will be added to the template description.
-//TemplateOverviewDescription::set_image_source_folder("_dev/designs");
-
-// --- help files ---
-//CMSHelp::set_help_file_directory_name("_help");
-//LeftAndMain::$help_link = "admin/help/";
-//===================---------------- END templateoverview MODULE ----------------===================
 
