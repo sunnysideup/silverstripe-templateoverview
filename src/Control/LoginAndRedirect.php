bd7f3b3610c028311a8fe8f1e0df178d28082954
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-05-20 10:41:19 +1200

    PATCH: adding more security

--- src/Control/LoginAndRedirect.php
+++ src/Control/LoginAndRedirect.php
@@ -4,9 +4,11 @@ namespace Sunnysideup\TemplateOverview\Control;
 
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
+use SilverStripe\Control\Util\IPUtils;
 use Sunnysideup\TemplateOverview\Tasks\CheckAllTemplates;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Core\Environment;
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Security\Security;
 use SilverStripe\Security\MemberAuthenticator\CookieAuthenticationHandler;
 
@@ -14,6 +16,10 @@ class LoginAndRedirect extends Controller
 {
     private static $allowed_actions = [
         'login' => '->isDev'
+    ];
+
+    private static $allowed_ips = [
+        '127.0.0.1'
     ];
 
     function login($request)
@@ -30,7 +36,12 @@ class LoginAndRedirect extends Controller
     public function isDev()
     {
         if(Environment::getEnv('ALLOW_SMOKE_TEST')) {
-            return Director::isDev();
+            $allowedIPs = Config::inst()->get(self::class, 'allowed_ips');
+            if(IPUtils::checkIP($this->request->getIP(), $allowedIPs)) {
+                return Director::isDev();
+            } else {
+                die('Please include your ip address in LoginAndRedirect.allowed_ips: '.$this->request->getIP(). '. Currently set are: '.implode(', ', $allowedIPs));
+            }
         } else {
             die('Please set ALLOW_SMOKE_TEST in your environment variables to use this service.');
         }
