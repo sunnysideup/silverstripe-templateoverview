14229fa89ad4584d3f8f25adf6d65ba3134287f4
Nicolaas <github@sunnysideup.co.nz>
2022-01-18 13:11:51 +1300

    PATCH: removing superfluous file

--- _config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * developed by www.sunnysideup.co.nz
- * author: Nicolaas - modules [at] sunnysideup.co.nz
- *
- */
-
-// Config::modify()->update("ErrorPage", 'icon', "templateoverview/images/treeicons/ErrorPage");
